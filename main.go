e7064a11e0368eb854b4d8b9425928ea27ec5793
Jared Gore <jared@madstone.io>
2022-10-23 17:55:44 -0400

    added markdown headers to file, loop through sheets in XLSX and make files for each

--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/xuri/excelize/v2"
-	"github.com/olekukonko/tablewriter"
 	"os"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/xuri/excelize/v2"
 	//"bufio"
 )
 
@@ -21,34 +22,47 @@ func main() {
 		}
 	}()
 
-	rows, err := f.GetRows("Book 1")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	data := [][]string{}
-
-	for i, row := range rows {
-		if i == 0 {
-			continue
+	for index, name := range f.GetSheetMap() {
+		data := [][]string{}
+		fmt.Println(index)
+		rows, err := f.GetRows(name)
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
-		data = append(data, []string{row[1], row[2], row[3], row[4]})
-	}	
 
-	mkdoc, err := os.OpenFile("SEC510-Index.md", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-	if err != nil {
-		fmt.Println(err)
-		return
+		for i, row := range rows {
+			if i == 0 {
+				continue
+			}
+			data = append(data, []string{row[1], row[2], row[3], row[4]})
+		}
+
+		fileName := fmt.Sprintf("SEC510 Index %v.md", name)
+
+		mkdoc, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer mkdoc.Close()
+
+		header := fmt.Sprintf("# %v\n", name)
+
+		pageHeader, err := mkdoc.WriteString(header)
+		if err != nil {
+			panic(err)
+			fmt.Println(pageHeader)
+		}
+
+		table := tablewriter.NewWriter(mkdoc)
+		table.SetAutoWrapText(false)
+		table.SetHeader([]string{"Page", "Slide Title", "Keyword 1", "Keyword 2"})
+		table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+		table.SetCenterSeparator("|")
+		table.AppendBulk(data)
+		table.Render()
+
 	}
-	defer mkdoc.Close()
-	
-	table := tablewriter.NewWriter(mkdoc)
-	table.SetAutoWrapText(false)
-	table.SetHeader([]string{"Page", "Slide Title", "Keyword 1", "Keyword 2"})
-	table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
-	table.SetCenterSeparator("|")
-	table.AppendBulk(data)
-	table.Render()
 
 }
