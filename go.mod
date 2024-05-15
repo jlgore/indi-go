c1950d60181dc356c3043c9963637a9e9da60a10
jg <jg@gore.cc>
2024-05-14 19:57:21 -0500

    shuold have done this last week

--- go.mod
+++ go.mod
@@ -3,18 +3,17 @@ module github.com/jlgore/giacronym
 go 1.17
 
 require (
+	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.9.2
 	github.com/olekukonko/tablewriter v0.0.5
 	github.com/xuri/excelize/v2 v2.6.1
+	github.com/yuin/goldmark v1.7.1
 )
 
 require (
-	github.com/jung-kurt/gofpdf v1.16.2 // indirect
-	github.com/mandolyte/mdtopdf v1.3.0 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.3 // indirect
-	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/xuri/efp v0.0.0-20220603152613-6918739fd470 // indirect
 	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
 	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
--- main.go
+++ main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 
+	wkhtmltopdf "github.com/SebastiaanKlippert/go-wkhtmltopdf"
 	"github.com/olekukonko/tablewriter"
 	"github.com/xuri/excelize/v2"
+	"github.com/yuin/goldmark"
+	"github.com/yuin/goldmark/extension"
+	"github.com/yuin/goldmark/renderer/html"
 	//"bufio"
 )
 
 func main() {
-	f, err := excelize.OpenFile("SEC510 Index.xlsx")
+	f, err := excelize.OpenFile("Index SEC541.xlsx")
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -35,10 +40,18 @@ func main() {
 			if i == 0 {
 				continue
 			}
-			data = append(data, []string{row[1], row[2], row[3], row[4]})
+			rowData := []string{}
+			for j := 1; j <= 4; j++ {
+				if j < len(row) {
+					rowData = append(rowData, row[j])
+				} else {
+					rowData = append(rowData, "")
+				}
+			}
+			data = append(data, rowData)
 		}
 
-		fileName := fmt.Sprintf("SEC510 Index %v.md", name)
+		fileName := fmt.Sprintf("Index SEC541 %v.md", name)
 
 		mkdoc, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 		if err != nil {
@@ -49,20 +62,96 @@ func main() {
 
 		header := fmt.Sprintf("# %v\n", name)
 
-		pageHeader, err := mkdoc.WriteString(header)
+		_, err = mkdoc.WriteString(header)
 		if err != nil {
-			panic(err)
-			fmt.Println(pageHeader)
+			fmt.Println(err)
+			return
 		}
 
+		// Create a new table writer
 		table := tablewriter.NewWriter(mkdoc)
-		table.SetAutoWrapText(false)
-		table.SetHeader([]string{"Page", "Slide Title", "Keyword 1", "Keyword 2"})
+		table.SetHeader([]string{"PAGE", "SLIDE TITLE", "KEYWORD 1", "KEYWORD 2"})
 		table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
 		table.SetCenterSeparator("|")
-		table.AppendBulk(data)
+		table.SetAutoWrapText(false)
+		table.SetAlignment(tablewriter.ALIGN_LEFT)
+
+		// Append the data rows to the table
+		for _, row := range data {
+			table.Append(row)
+		}
+
+		// Render the table
 		table.Render()
 
-	}
+		// Read the Markdown file
+		mdFile, err := os.ReadFile(fileName)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
 
+		// Convert markdown to HTML
+		var buf bytes.Buffer
+		md := goldmark.New(
+			goldmark.WithExtensions(extension.Table),
+			goldmark.WithRendererOptions(
+				html.WithUnsafe(),
+			),
+		)
+		if err := md.Convert(mdFile, &buf); err != nil {
+			fmt.Println(err)
+			return
+		}
+		html := buf.String()
+
+		// Create a new PDF generator
+		pdfg, err := wkhtmltopdf.NewPDFGenerator()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// Set the HTML content with custom CSS styles
+		htmlWithStyles := fmt.Sprintf(`
+    <html>
+    <head>
+        <style>
+            table {
+                border-collapse: collapse;
+                width: 100%%;
+            }
+            th, td {
+                border: 1px solid black;
+                padding: 8px;
+                word-wrap: break-word;
+                white-space: normal;
+            }
+        </style>
+    </head>
+    <body>
+        %s
+    </body>
+    </html>
+`, html)
+		pdfg.AddPage(wkhtmltopdf.NewPageReader(bytes.NewReader([]byte(htmlWithStyles))))
+		// Set the PDF file path
+		pdfFileName := fmt.Sprintf("Index SEC541 %v.pdf", name)
+
+		// Create a new file
+		file, err := os.Create(pdfFileName)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+
+		// Set the output to the file
+		pdfg.SetOutput(file)
+
+		// Generate the PDF file
+		if err := pdfg.Create(); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
 }
